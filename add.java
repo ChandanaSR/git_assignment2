1fbea1ce9f8aa687d0a7b1d7c14f467862c07479
ChandanaSR <60378393+ChandanaSR@users.noreply.github.com>
2021-08-24 18:59:51 +0530

    4x commit

---
+++ add.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class add {
+
+    public static void main(String[] args) {
+        
+        int x, y, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        x = sc.nextInt();
+        
+        System.out.println("Enter Second Number: ");
+        y = sc.nextInt();
+        
+        sc.close();
+        sum = 4x + 4y;
+        System.out.println("Sum of these numbers: "+sum);
+    }
+}
+
